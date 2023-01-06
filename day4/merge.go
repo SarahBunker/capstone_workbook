e5ba90cf22a8eef9235209ad228b27d7a1246e66
Sarah Bunker <ChemEnCoder@gmail.com>
2023-01-05 19:15:00 -0700

    completing merge

--- day4/merge.go
+++ day4/merge.go
@@ -29,11 +29,40 @@ func createList(nums []int) (head *ListNode) {
 }
 
 func main() {
-	s1 := []int{1, 2, 3, 4, 5}
+	println("simple merge")
+	s1 := []int{1, 3, 5}
 	l1 := createList(s1)
-	n1 := []int{1, 2, 3, 4, 5}
+	n1 := []int{2, 4, 6}
 	l2 := createList(n1)
 	l3 := mergeTwoLists(l1, l2)
+	printList(l3)
+
+	println("first: 1 1 2 3 4 4")
+	s1 = []int{1, 2, 4}
+	l1 = createList(s1)
+	n1 = []int{1, 3, 4}
+	l2 = createList(n1)
+	l3 = mergeTwoLists(l1, l2)
+	printList(l3)
+
+	println("empty")
+	l1 = &ListNode{}
+	l2 = &ListNode{}
+	l3 = mergeTwoLists(l1, l2)
+	printList(l3)
+
+	println("first list empty")
+	l1 = &ListNode{}
+	n1 = []int{1, 3, 4}
+	l2 = createList(n1)
+	l3 = mergeTwoLists(l1, l2)
+	printList(l3)
+
+	println("second list empty")
+	s1 = []int{1, 2, 4}
+	l1 = createList(s1)
+	l2 = &ListNode{}
+	l3 = mergeTwoLists(l1, l2)
 	printList(l3)
 
 	// s = []int{1, 2, 3, 4, 5}
@@ -55,17 +84,38 @@ func main() {
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	dummy := &ListNode{}
 	c := dummy
-	for c != nil {
+	if list1 == nil {
+		c.Next = list2
+		return dummy.Next
+	}
+
+	if list2 == nil {
+		c.Next = list1
+		return dummy.Next
+	}
+
+	for list1 != nil || list2 != nil {
 		if list1.Val <= list2.Val {
 			c.Next = list1
 			list1 = list1.Next
 			c = c.Next
 		}
 
+		if list1 == nil {
+			c.Next = list2
+			return dummy.Next
+		}
+
 		if list2.Val < list1.Val {
+
 			c.Next = list2
 			list2 = list2.Next
 			c = c.Next
+		}
+
+		if list2 == nil {
+			c.Next = list1
+			return dummy.Next
 		}
 
 	}
