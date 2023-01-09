ef6b9ba269e93f9ec353edf553e1f24870bd8eba
Sarah Bunker <ChemEnCoder@gmail.com>
2023-01-09 08:44:20 -0700

    adding files for tuesday, writing idea for jump

--- w2d2/jump_game.go
+++ w2d2/jump_game.go
@@ -12,9 +12,28 @@ func canJump(nums []int) bool {
 }
 
 /*
+
+iterative call
+
+for the current index
+if current index is last index
+  return true
+if  current index is 0
+	return false
+if current index included in memoize
+	return value from memo
+if curr index not in memoize
+	add return of current index to memoize
+	call function with each index reachable by current value
+	return || of each recursive call function
+
+
 start at index 0
 memoize all the indices you have visited
 go to all the indices
 
 base case getting to last index
+
+start at last index
+find
 */
