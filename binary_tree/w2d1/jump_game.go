957b73b65a1d40c829031283e0b49cfa4cfe0729
Sarah Bunker <sarah.bunker14@gmail.com>
2023-01-09 07:21:01 -0800

    starting problems for next week

---
+++ binary_tree/w2d1/jump_game.go
@@ -0,0 +1,20 @@
+package main
+
+// import "fmt"
+
+func main() {
+
+}
+
+func canJump(nums []int) bool {
+
+	return false
+}
+
+/*
+start at index 0
+memoize all the indices you have visited
+go to all the indices
+
+base case getting to last index
+*/
