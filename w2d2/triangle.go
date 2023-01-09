1918f372c0e203b3d0d62bbec509be87578689ae
Sarah Bunker <ChemEnCoder@gmail.com>
2023-01-09 16:44:23 -0700

    switching to solving iteratively, current solution for triangle times out on a few test cases

--- w2d2/triangle.go
+++ w2d2/triangle.go
@@ -4,6 +4,42 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello")
+	triangle := [][]int{
+		{2},
+		{3, 4},
+		{6, 5, 7},
+		{4, 1, 8, 3},
+		{1, 1, 1, 1, 1},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0}}
+	res := minimumTotal(triangle)
+	fmt.Println(res)
+
+}
+
+func minimumTotal(triangle [][]int) int {
+	memo := map[[2]int]int{}
+	return minSum(triangle, 0, 0, memo)
+}
+
+func minSum(triangle [][]int, row, col int, memo map[[2]int]int) int {
+	rowMax := len(triangle) - 1
+	if row == rowMax {
+		return triangle[row][col]
+	}
+
+	directBottom := minSum(triangle, row+1, col, memo)
+	oneLeftBottom := minSum(triangle, row+1, col+1, memo)
+	res := min(directBottom, oneLeftBottom) + triangle[row][col]
+	memo[[2]int{row, col}] = res
+	return res
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
 
 /*
@@ -15,6 +51,10 @@ base case
 
 memo < store the best sum at each index of the last row
 
+base
+	sum at bottom row is value at current index
 
+recursion
+	return min()
 
 */
